fe523fe061a9152f62a7d33d85204a81a1a4a7d2
Augy <ateoks@gmail.com>
2024-08-27 19:26:01 +0800

    fix 3k

--- private-messaging/src/socket.js
+++ private-messaging/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://private-chat.augy.xyz:3000";
+const URL = "https://private-chat.augy.xyz";
 const socket = io(URL, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
